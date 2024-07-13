47de3040d8631a9ccc7174bee8aad1f7a6d4e5ea
xDohax <LeeDoha@proton.me>
2024-07-14 00:33:11 +0900

    edit wrong indent(though not problem)

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-    function updateTime() {
-    const now = new Date();
-    const menuClockElement = document.getElementById('menuClock');
-    const dateElement = document.getElementById('date');
-    const timeElement = document.getElementById('time');
-
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-
-    const options = { weekday: 'short', month: 'long', day: 'numeric' };
-
-    menuClockElement.textContent = `${now.toLocaleDateString('US-us', options)} ${hours}:${minutes}`;
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     function updateTime() {
         const now = new Date();
         const dateElement = document.getElementById('date');
